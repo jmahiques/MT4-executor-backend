86eb3954a60ac372b058257d4acd2a5c9d5a52bc
Jordi <jordi@acceseo.com>
2020-01-05 19:16:24 +0100

    Actualiza los archivos de entrada a la aplicación de dev y prod

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\EndPoint\EndPoint;
 use App\Router\Router;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
-use RingCentral\Psr7\Response;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -18,15 +16,8 @@ $creator = new ServerRequestCreator(
 );
 $serverRequest = $creator->fromGlobals();
 
-$response = new Response(404);
-if ($serverRequest->getUri()->getPath() === '/index.php/open') {
-    $response = (new OpenPosition())->execute($serverRequest);
-} elseif ($serverRequest->getUri()->getPath() === '/index.php/tick') {
-    $response = (new MarketTick())->execute($serverRequest);
-} elseif ($serverRequest->getUri()->getPath() === '/index.php/probe') {
-    $response = (new Probe())->execute($serverRequest);
-} elseif ($serverRequest->getUri()->getPath() === '/index.php/recreate') {
-    $response = (new Probe())->execute($serverRequest);
-}
+$response = (new Router('/index.php'))
+    ->match($serverRequest->getUri()->getPath())
+    ->execute($serverRequest);
 
 (new SapiEmitter())->emit($response);
--- react.php
+++ react.php
@@ -1,10 +1,27 @@
 <?php
 
-use App\Utils\Server;
+use App\Router\Router;
+use Psr\Http\Message\ServerRequestInterface;
+use React\EventLoop\Factory;
+use React\Http\Server as ReactPHPServer;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$redis = new Redis();
-$redis->connect('redis');
+$loop = Factory::create();
+$server = new ReactPHPServer(function (ServerRequestInterface $serverRequest) {
+    return (new Router('/react.php'))
+        ->match($serverRequest->getUri()->getPath())
+        ->execute($serverRequest);
+});
 
-Server::initialize();
+$server->on('error', function (\Throwable $e) {
+    echo 'Error: ' . $e->getMessage() . ' --' . $e->getTraceAsString() . PHP_EOL;
+    echo 'File: '.$e->getFile().' Line: '.$e->getLine().' Code: '.$e->getCode() . PHP_EOL;
+});
+
+$socket = new \React\Socket\Server('0.0.0.0:8080', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
+
+$loop->run();
